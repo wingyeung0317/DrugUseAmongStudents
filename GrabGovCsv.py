5f406e2720f2274c0db96db854d3473eb7e39803
Wing <wingyeung0317@hotmail.com>
2023-12-08 08:03:25 +0800

    .step: progress

--- GrabGovCsv.py
+++ GrabGovCsv.py
@@ -3,6 +3,9 @@ import pandas as pd
 import urllib.request
 from bs4 import BeautifulSoup as soup
 from dataclasses import dataclass
+import re
+from IPython.display import display
+
 
 class govDataset:
     hdr = {'User-Agent':'Mozilla/5.0', 'Accept': 'text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
@@ -13,7 +16,7 @@ class govDataset:
 
         self._connected = 0
         self.html_soup = soup
-        self._allTags = soup.ResultSet
+        self.df = pd.DataFrame()
         self.connect()
 
     def connect(self):
@@ -23,27 +26,50 @@ class govDataset:
             self.html_page = urllib.request.urlopen(self.req, timeout=60)
             self.html_soup = soup(self.html_page, "html.parser")
             self._connected = 1
+            print('Connected')
         except:
             print(f'Failed connecting to {self.dataURL}')
 
-    class grab:
-        def __init__(self, mode:str='print'):
-            self.mode = mode
+    def grab(self, mode:str, lang:str='all'):
+        """ 
+        mode='print' or
+        mode='csv' or
+        mode='txt'
 
-        def grabCsvList(self):
-            if (self._connected):
-                self.htmlTagToKeep = ['a']
-                self._allTags = self.html_soup.findAll(self.htmlTagToKeep, class_='dataset-details__list-item-download')
+        lang='all' or
+        lang='zh' or
+        lang='en'
+        """
+        self.mode = mode
+        if (self._connected):
+            self._htmlTagToKeep = ['a']
+            self._allTitle = self.html_soup.findAll(self._htmlTagToKeep, class_='dataset-details__list-item-download')
+            self._allaTags = self.html_soup.findAll(self._htmlTagToKeep, class_='dataset-details__list-item-download')
+            
+            for i, title in enumerate(self._allTitle):
+                self.url[i] = title
 
-        def save(self):
-            if self.mode=='print':
-                for i, tag in enumerate(self._allTags):
-                    print(tag.get('href'))
-                    self.url[i] = tag
+            self.df['Title'] = self.url
+            # PROGRESS
+            
 
-            if self.mode=='save':
-                self.f = open('csvList.txt', 'w')
-                for i, tag in enumerate(self._allTags):
-                    self.url[i] = tag
-                    self.f.write(tag.get('href')+'\n')
-                self.f.close()
+            self.save(lang)
+
+    def save(self):
+        if self.mode=='dataframe':
+            display(self.df);
+        if self.mode=='print':
+            for tag in self._allaTags:
+                print(tag.get('href'))
+
+        if self.mode=='txt':
+            self.f = open('csvList.txt', 'w')
+            for tag in self._allaTags:
+                self.f.write(tag.get('href')+'\n')
+            self.f.close()
+
+        if self.mode == 'csv':
+            for tag in self._allaTags:
+                local_filename, headers = urllib.request.urlretrieve('tag')
+                urllib.request.urlretrieve(re.findall(r'ss[0-9]{4}.*',local_filename, f'{local_filename}.csv'))
+            self.f.close()
--- download.py
+++ download.py
@@ -1,5 +1,7 @@
 import GrabGovCsv
-
+print('Please enter the relative data.gov.hk link')
 link = input()
 govData = GrabGovCsv.govDataset(link)
-govData.grab()
+print('Please choose a type: Print / csv / txt\nPrint would print all link to the console.\ncsv would download the csv files\ntxt would print all the csv links into ./csvList.txt')
+method = input().lower()
+govData.grab(method)
